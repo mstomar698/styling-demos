cfb09742e4f265df66bdc166a60c77a37ccf3da5
mstomar698 <tomarm698@gmail.com>
2023-02-07 00:01:28 +0530

    Adding Card

---
+++ card_component/src/app/page.js
@@ -0,0 +1,31 @@
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <div className="box">
+      <div className="in_box">
+        <div className="card_img">
+          <div className="card_content">
+            <h1>Content Head</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+            </p>
+            <p>
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum. of type and scrambled it to make a type specimen
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
